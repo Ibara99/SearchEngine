3c05cd8e16a510c5245f3d03100ed9b2cb33c729
ibara99 <arsip.ibara@gmail.com>
2019-11-27 07:06:18 +0700

    fix some bug

--- website/hello.py
+++ website/hello.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
 import pre_v1 as p
 app = Flask(__name__)
@@ -32,37 +31,3 @@ def login():
 		return render_template('hasil.html', index=hasil)
 		# return '<h2>10 artikel terkait adalah artikel ke : ' + tmp+'</h2>'
 	return render_template('login.html')
-=======
-from flask import Flask, render_template, request
-import pre_v1 as p
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-# klo statis
-@app.route('/setting')
-def setting():
-    return 'Halaman setting'
-
-# klo dinamis
-@app.route('/profile/<nama>')
-def profil(nama):
-    return render_template('template_dinamis.html', username=nama)
-# klo dinamis
-@app.route('/profile2/<int:id>')
-def profil2(id):
-    return f'Hello, ID kamu adalah {id}'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if request.method== 'POST':
-		query = request.form['email']
-		hasil = p.cari(query)
-		tmp =""
-		for x in hasil:
-			tmp += str(x) + ", "
-		return '<h2>10 artikel terkait adalah artikel ke : ' + tmp+'</h2>'
-	return render_template('login.html')
->>>>>>> 5c68a5fa11b6d2fd5708367abf40d0d78e136afc
